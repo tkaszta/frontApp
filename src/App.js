18670266115653b058f982e3bd7015535410c5ab
tkaszta <tkaszta@gmail.com>
2023-02-09 20:40:23 +0200

    app with back

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import './App.css';
+import logo from './logo.svg';
 
-function App() {
-  const value = 'World Alma';
-  return <div>Hello {value}</div>;
+const App = () => {
+
+  const [groups, setGroups] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setLoading(true);
+
+    fetch('/viragok')
+      .then(response => response.json())
+      .then(data => {
+        setGroups(data);
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-intro">
+          <h2>Virag Lista</h2>
+          {
+            groups.map(group =>
+            <div key={group.id}>
+              {group.name}
+            </div>
+          )}
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
